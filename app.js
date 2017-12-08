c900bbaf54cebf5a0965013e569e5fa4140c7d25
Taylor Stemple <taylorstemple@gmail.com>
2017-12-08 14:53:02 -0800

    finished sum and multipy func

--- app.js
+++ app.js
@@ -56,11 +56,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // TODO: Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var theNextProduct = multiply(a,b);
+  theNextProduct = multiply(theNextProduct[0], c);
+  theNextProduct = theNextProduct[0];
+  var nextSum = sum(a, b);
+  nextSum = sum(nextSum[0], c);
+  nextSum = nextSum[0];
+  var sumMessage = a + ' and ' + b + ' and ' + c + ' sum to ' + nextSum +'.';
+  var productMessage = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + theNextProduct + '.';
+  return [nextSum, theNextProduct, sumMessage, productMessage];
 }
 
 // TODO: Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
