9d40dd5c0dadf58232420aebb2832d076381d251
Taylor Stemple <taylorstemple@gmail.com>
2017-12-08 15:33:27 -0800

    wrote function to find sum of array

--- app.js
+++ app.js
@@ -86,11 +86,24 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
+  var arrSum = 0;
+   var arrMessage = '';
+
+  for(var i = 0;i < testArray.length; i++) {
+    arrSum = sum(arrSum,testArray[i]);
+    arrSum = arrSum[0];
+    arrMessage += testArray[i];
+    if ( i !== testArray.length - 1) arrMessage += ',';
+    }
+
+    arrMessage += ' was passed in as an array of numbers, and ' + arrSum + ' is their sum.';
+    return [arrSum, arrMessage];
 
 }
 
+
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+ testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
